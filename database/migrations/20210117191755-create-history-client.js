ca218645d024b339e16fe289388f4ead8c398761
goncalveshigino <goncalveshigono20@gmail.com>
2021-01-19 01:17:27 +0100

    kk

--- database/migrations/20210117191755-create-history-client.js
+++ database/migrations/20210117191755-create-history-client.js
@@ -1,44 +1,49 @@
 'use strict';
 module.exports = {
-    up: async(queryInterface, Sequelize) => {
-        await queryInterface.createTable('HistoryClients', {
-            id: {
-                allowNull: false,
-                autoIncrement: true,
-                primaryKey: true,
-                type: Sequelize.INTEGER
-            },
-            clientId: {
-                type: Sequelize.INTEGER,
-                allowNull: false,
-
-            },
-            employeeId: {
-                type: Sequelize.INTEGER,
-                allowNull: false,
-                references: {
-                    model: 'Employees',
-                    key: 'id',
-                },
-                onUpdate: 'CASCADE',
-                onDelete: 'CASCADE',
-
-            },
-            description: {
-                type: Sequelize.STRING
-            },
-
-            createdAt: {
-                allowNull: false,
-                type: Sequelize.DATE
-            },
-            updatedAt: {
-                allowNull: false,
-                type: Sequelize.DATE
-            }
-        });
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('HistoryClients', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      clientId: {
+        type: Sequelize.INTEGER,
+        allowNull:false,
+        references: {
+          model: 'Clients',
+          key: "id"
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+      },
+      employeeId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Employees',
+          key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    
+      },
+      description: {
+        type: Sequelize.STRING
     },
-    down: async(queryInterface, Sequelize) => {
-        await queryInterface.dropTable('HistoryClients');
-    }
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('HistoryClients');
+  }
 };
