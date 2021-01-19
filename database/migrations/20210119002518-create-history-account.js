09e38c886ae1735a6a4df5ecab5bbb0d143978ff
goncalveshigino <goncalveshigono20@gmail.com>
2021-01-19 08:33:41 +0100

    feito

---
+++ database/migrations/20210119002518-create-history-account.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+    up: async(queryInterface, Sequelize) => {
+        await queryInterface.createTable('HistoryAccounts', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            accountId: {
+                type: Sequelize.INTEGER
+            },
+            employeeId: {
+                type: Sequelize.INTEGER
+            },
+            description: {
+                type: Sequelize.TEXT
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async(queryInterface, Sequelize) => {
+        await queryInterface.dropTable('HistoryAccounts');
+    }
+};
